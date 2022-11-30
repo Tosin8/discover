f090a55dc43b944174f37cbfa6c56bd54deac9d9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-30 22:12:37 +0100

    Updating Body with SingleChildScrollView

--- lib/Screens/home_page/components/body.dart
+++ lib/Screens/home_page/components/body.dart
@@ -12,108 +12,110 @@ class Body_HomePage extends StatefulWidget {
 class _Body_HomePageState extends State<Body_HomePage> {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
+    return SingleChildScrollView(
+      child: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        children: [
+          Card(
+              elevation: 2,
+              shadowColor: Colors.grey,
+              child: Column(
+                children: [
+                  Image.asset('', height: 10),
+                  Container(
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Container(
+                            color: kPrimaryColor,
+                            decoration:
+                                const BoxDecoration(shape: BoxShape.circle),
+                          ),
+                          const Text(
+                            'Social \n Illustration',
+                            style:
+                                TextStyle(color: kSecondaryColor, fontSize: 12),
+                          ),
+                        ],
+                      )),
+                ],
+              )),
+          Card(
+              elevation: 2,
+              shadowColor: Colors.grey,
+              child: Column(
+                children: [
+                  Image.asset('', height: 10),
+                  Container(
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Container(
+                            color: kPrimaryColor,
+                            decoration:
+                                const BoxDecoration(shape: BoxShape.circle),
+                          ),
+                          const Text(
+                            'Ecommerce \n Illustration',
+                            style:
+                                TextStyle(color: kSecondaryColor, fontSize: 12),
+                          ),
+                        ],
+                      )),
+                ],
+              )),
+          Card(
+              elevation: 2,
+              shadowColor: Colors.grey,
+              child: Column(
+                children: [
+                  Image.asset('', height: 10),
+                  Container(
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Container(
+                            color: kPrimaryColor,
+                            decoration:
+                                const BoxDecoration(shape: BoxShape.circle),
+                          ),
+                          const Text(
+                            'Coding \n Illustration',
+                            style:
+                                TextStyle(color: kSecondaryColor, fontSize: 12),
+                          ),
+                        ],
+                      )),
+                ],
+              )),
+          Card(
+              elevation: 2,
+              shadowColor: Colors.grey,
+              child: Column(
+                children: [
+                  Image.asset('', height: 10),
+                  Container(
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Container(
+                            color: kPrimaryColor,
+                            decoration:
+                                const BoxDecoration(shape: BoxShape.circle),
+                          ),
+                          const Text(
+                            'Business \n Illustration',
+                            style:
+                                TextStyle(color: kSecondaryColor, fontSize: 12),
+                          ),
+                        ],
+                      )),
+                ],
+              )),
+        ],
       ),
-      children: [
-        Card(
-            elevation: 2,
-            shadowColor: Colors.grey,
-            child: Column(
-              children: [
-                Image.asset('', height: 10),
-                Container(
-                    color: Colors.white,
-                    child: Row(
-                      children: [
-                        Container(
-                          color: kPrimaryColor,
-                          decoration:
-                              const BoxDecoration(shape: BoxShape.circle),
-                        ),
-                        const Text(
-                          'Social \n Illustration',
-                          style:
-                              TextStyle(color: kSecondaryColor, fontSize: 12),
-                        ),
-                      ],
-                    )),
-              ],
-            )),
-        Card(
-            elevation: 2,
-            shadowColor: Colors.grey,
-            child: Column(
-              children: [
-                Image.asset('', height: 10),
-                Container(
-                    color: Colors.white,
-                    child: Row(
-                      children: [
-                        Container(
-                          color: kPrimaryColor,
-                          decoration:
-                              const BoxDecoration(shape: BoxShape.circle),
-                        ),
-                        const Text(
-                          'Ecommerce \n Illustration',
-                          style:
-                              TextStyle(color: kSecondaryColor, fontSize: 12),
-                        ),
-                      ],
-                    )),
-              ],
-            )),
-        Card(
-            elevation: 2,
-            shadowColor: Colors.grey,
-            child: Column(
-              children: [
-                Image.asset('', height: 10),
-                Container(
-                    color: Colors.white,
-                    child: Row(
-                      children: [
-                        Container(
-                          color: kPrimaryColor,
-                          decoration:
-                              const BoxDecoration(shape: BoxShape.circle),
-                        ),
-                        const Text(
-                          'Coding \n Illustration',
-                          style:
-                              TextStyle(color: kSecondaryColor, fontSize: 12),
-                        ),
-                      ],
-                    )),
-              ],
-            )),
-        Card(
-            elevation: 2,
-            shadowColor: Colors.grey,
-            child: Column(
-              children: [
-                Image.asset('', height: 10),
-                Container(
-                    color: Colors.white,
-                    child: Row(
-                      children: [
-                        Container(
-                          color: kPrimaryColor,
-                          decoration:
-                              const BoxDecoration(shape: BoxShape.circle),
-                        ),
-                        const Text(
-                          'Business \n Illustration',
-                          style:
-                              TextStyle(color: kSecondaryColor, fontSize: 12),
-                        ),
-                      ],
-                    )),
-              ],
-            )),
-      ],
     );
   }
 }

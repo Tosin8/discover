e5e654b04e4492f691aff7f0f05966075f09c77e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-30 21:54:03 +0100

    Updating body with GridView

--- lib/Screens/home_page/components/body.dart
+++ lib/Screens/home_page/components/body.dart
@@ -10,6 +10,16 @@ class Body_HomePage extends StatefulWidget {
 class _Body_HomePageState extends State<Body_HomePage> {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder();
+    return GridView(
+  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    crossAxisCount: 3,
+  ),
+  children: [
+    Image.network('https://picsum.photos/250?image=1'),
+    Image.network('https://picsum.photos/250?image=2'),
+    Image.network('https://picsum.photos/250?image=3'),
+    Image.network('https://picsum.photos/250?image=4'),
+  ],
+)
   }
 }
